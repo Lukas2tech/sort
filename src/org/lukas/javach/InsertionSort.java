654b8b954ddb1dfe2fd257fefb0a969404ac3645
LukasPecak <lukas.pecak@gmail.com>
2016-04-02 00:04:04 +0200

    Javadoc added

--- src/org/lukas/javach/InsertionSort.java
+++ src/org/lukas/javach/InsertionSort.java
@@ -1,9 +1,18 @@
 package org.lukas.javach;
 
+/**
+ * @author Lukas
+ *
+ */
+
 public class InsertionSort {
 	private static final int SIZE = 10;
 	private static final int BOUND = 100;
 	
+	/**
+	 * 
+	 * @param args Command line parameters
+	 */
 	public static void main(String[] args){
 		BubleSort buble = new BubleSort();
 		int[] array = buble.genTable(SIZE, BOUND);
@@ -13,6 +22,11 @@ public class InsertionSort {
 		buble.printTable(result);
 	}
 	
+	/**
+	 * 				
+	 * @param array The array that is going to be sorted.
+	 * @return Return the sorted array object.
+	 */
 	public int[] sort(int[] array){
 		int[] result = new int[SIZE];
 		int position = 0;
@@ -29,6 +43,12 @@ public class InsertionSort {
 		return result;
 	}
 	
+	/**
+	 * 
+	 * @param clearPosition The position that is need to be cleared for element insertion.
+	 * @param array The sorted result array.
+	 * @return Returns the sorted array with free space for insert the searched element.
+	 */
 	private int[] moveElements(int clearPosition, int[] array){
 		for(int i = array.length-1; i > clearPosition; i--){
 			array[i] = array[i-1];
@@ -36,6 +56,13 @@ public class InsertionSort {
 		return array;
 	}
 	
+	/**
+	 * 
+	 * @param array The sorted result array.
+	 * @param element The element that is going to be inserted into the sorted array.
+	 * @param count The number of elements that are already in the sorted array.
+	 * @return Returns the position where the element is going to be inserted.
+	 */
 	private int findPosition(int[] array, int element, int count){
 		for(int i = 0; i < count; i++){
 			if(array[i] > element){
